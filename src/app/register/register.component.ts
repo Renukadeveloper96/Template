55210a157a2b40901da1a93fcfca8681b6cfdf11
Renukadeveloper96 <renukamaradagi@gmail.com>
2023-09-17 10:41:40 +0530

    Profile

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from '../_services/auth.service';
 export class RegisterComponent implements OnInit {
   form: any = {
     username: null,
+    lastname:null,
     email: null,
     password: null
   };
@@ -22,9 +23,9 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
-    const { username, email, password } = this.form;
+    const { username,lastname, email, password } = this.form;
 
-    this.authService.register(username, email, password).subscribe({
+    this.authService.register(username,lastname,email,password).subscribe({
       next: data => {
         console.log(data);
         this.isSuccessful = true;
